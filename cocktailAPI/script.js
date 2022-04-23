3ed33e8d4472f8a44f4044acb121c7e7d491f92a
Jonathan Mani <jonathanmani@protonmail.com>
2022-04-22 22:09:25 -0400

    styled the html, added more calls to the fetch request

--- cocktailAPI/script.js
+++ cocktailAPI/script.js
@@ -1,16 +1,23 @@
-fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
-.then(res => res.json())
-.then(data => {
-    console.log(data)
-})
-.catch(err =>{
-    console.log('error ${err}')
-})
-
-
 document.querySelector('button').addEventListener('click', getDrink)
 
 function getDrink(){
+    let drink = document.querySelector('input').value;
+
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.drinks[0])
+            document.querySelector('#instructions').innerText = data.drinks[0].strInstructions
+            document.querySelector('.drinkImage').src = data.drinks[0].strDrinkThumb
+        })
+        .catch(err =>{
+            console.log('error ${err}')
+        })
+
     document.querySelector('h1').innerText = document.querySelector('input').value;
+
+    
+
+    
 }
 
