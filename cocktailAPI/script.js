3fb62352520303abb28a8ee3c09a87640f37efe0
Jonathan Mani <jonathanmani@protonmail.com>
2022-04-25 20:16:13 -0400

    made some aesthetic changes to the app

--- cocktailAPI/script.js
+++ cocktailAPI/script.js
@@ -6,9 +6,10 @@ function getDrink(){
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
         .then(res => res.json())
         .then(data => {
-            console.log(data.drinks[0])
-            document.querySelector('#instructions').innerText = data.drinks[0].strInstructions
+            console.log(data.drinks)
             document.querySelector('.drinkImage').src = data.drinks[0].strDrinkThumb
+            document.querySelector('#instructions').innerText = data.drinks[0].strInstructions
+            document.querySelector('h3').hidden = false;
         })
         .catch(err =>{
             console.log('error ${err}')
