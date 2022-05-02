612cd5e580960ede7c3463be3f4e0b5a32407d6d
Jonathan Mani <jonathanmani@protonmail.com>
2022-05-02 16:45:09 -0400

    added line to clear the drinks div element everytime a new search is made

--- cocktailAPI/script.js
+++ cocktailAPI/script.js
@@ -8,12 +8,15 @@ function getDrink(){
         .then(data => {
             console.log(data.drinks)
             let drinkDiv = document.querySelector('.drink-div');
+            
+            //clear everything in the drinkDiv before populating with a new drink
+            drinkDiv.innerHTML = '';
 
             for (let i = 0; i < data.drinks.length; i++){
                 let drinkImage = document.createElement('img');
                 drinkImage.setAttribute('class','drinkImage');
                 drinkImage.src = data.drinks[i].strDrinkThumb;
-                drinkDiv.appendChild(drinkImage);
+                drinkDiv.appendChild(drinkImage) ;
             }
             //document.querySelector('.drinkImage').src = data.drinks[0].strDrinkThumb
             //document.querySelector('#instructions').innerText = data.drinks[0].strInstructions
