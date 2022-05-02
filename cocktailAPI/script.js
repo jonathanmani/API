155cd9cba318c87d0419518d7b4499de7c1273af
Jonathan Mani <jonathanmani@protonmail.com>
2022-05-02 17:30:26 -0400

    changed some stylizing when the images display

--- cocktailAPI/script.js
+++ cocktailAPI/script.js
@@ -8,17 +8,32 @@ function getDrink(){
         .then(data => {
             console.log(data.drinks)
             let drinkDiv = document.querySelector('.drink-div');
-            
+
             //clear everything in the drinkDiv before populating with a new drink
             drinkDiv.innerHTML = '';
 
             for (let i = 0; i < data.drinks.length; i++){
+                //create a div that holds the drink image and drink text
+                let drinkContainer = document.createElement('div');
+                drinkContainer.setAttribute('class','drinkContainer');
+
+                //create an image element to hold the image
                 let drinkImage = document.createElement('img');
                 drinkImage.setAttribute('class','drinkImage');
                 drinkImage.src = data.drinks[i].strDrinkThumb;
-                drinkDiv.appendChild(drinkImage) ;
+
+                //create a h3 element to hold the drink name
+                let drinkName = document.createElement('h3');
+                drinkName.innerHTML = data.drinks[i].strDrink;
+
+                //append the image and name to the drinkContainer
+                drinkContainer.appendChild(drinkImage);
+                drinkContainer.appendChild(drinkName);
+
+                //append the drinkContainer to the drinkDiv element
+                drinkDiv.appendChild(drinkContainer)
             }
-            //document.querySelector('.drinkImage').src = data.drinks[0].strDrinkThumb
+            
             //document.querySelector('#instructions').innerText = data.drinks[0].strInstructions
             
         })
